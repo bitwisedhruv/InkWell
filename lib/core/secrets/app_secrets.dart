ca29d42788eb3cea33346046d85f3867ac54be4d
bitwisedhruv <monkcode463@gmail.com>
2024-03-28 01:05:57 +0530

    Login feature

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://wgafceffqejwyvroyjjd.supabase.co';
+  static const supabaseUrl = 'https://ncjjyxvsypgmyelxxrvg.supabase.co';
   static const supabseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnYWZjZWZmcWVqd3l2cm95ampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0NzMwNzgsImV4cCI6MjAyNzA0OTA3OH0.Y-b3Z9pIZo3dsXzX1KDd4CJPD54NQalQ40IkyznIxYg';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jamp5eHZzeXBnbXllbHh4cnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1NjAxMzMsImV4cCI6MjAyNzEzNjEzM30.3PGmFSr_h63acKL8LXvH0sDvINlUR6xAEL1t2j4CWZk';
 }
--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/data_sources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/use_cases/user_login.dart';
 import 'package:blog_app/features/auth/domain/use_cases/user_signup.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -37,9 +38,16 @@ void _initAuth() {
     ),
   );
 
+  serviceLocator.registerFactory<UserLogin>(
+    () => UserLogin(
+      serviceLocator(),
+    ),
+  );
+
   serviceLocator.registerLazySingleton(
     () => AuthBloc(
       userSignUp: serviceLocator(),
+      userLogin: serviceLocator(),
     ),
   );
 }
